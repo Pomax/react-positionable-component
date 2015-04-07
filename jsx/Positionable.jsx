0a935278d980d27b6db2db85913703afe87fecbf
Pomax <pomax@nihongoresources.com>
2015-04-07 16:51:59 -0700

    touch...?

--- jsx/Positionable.jsx
+++ jsx/Positionable.jsx
@@ -82,9 +82,9 @@ var Positionable = React.createClass({
   touchEnd: function(evt) { document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch end " + evt.touches[0].pageX + "/" + evt.touches[0].pageY }})); },
 */
 
-  touchStart: function(evt) { document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch start "+JSON.stringify(evt) }} )); },
-  touchMove: function(evt) { document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch move "+JSON.stringify(evt) }} )); },
-  touchEnd: function(evt) { document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch end "+JSON.stringify(evt) }} )); },
+  touchStart: function(evt) { evt = evt.nativeEvent; document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch start " + evt.touches[0].pageX + "/" + evt.touches[0].pageY }})); },
+  touchMove: function(evt) { evt = evt.nativeEvent; document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch move " + evt.touches[0].pageX + "/" + evt.touches[0].pageY }})); },
+  touchEnd: function(evt) { evt = evt.nativeEvent; document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch end " + evt.touches[0].pageX + "/" + evt.touches[0].pageY }})); },
 
 /*
   touchStart: function(evt) { document.dispatchEvent (new CustomEvent("app:log", {detail: { msg: "touch start " + evt.clientX + "/" + evt.clientY }})); },
